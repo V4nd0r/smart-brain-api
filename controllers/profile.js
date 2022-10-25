79c6fef5776b14c441943c66c6b71e6590ac545d
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2022-10-25 19:22:42 +0100

    Tidy up

---
+++ controllers/profile.js
@@ -0,0 +1,17 @@
+const handleProfileGet = (req, res, db) => {
+    const { id } = req.params; 
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if (user.lenghth){
+          response.json(user[0])  
+        }else{
+            res.status(404).json('Not found')
+        }
+    })
+    .catch( err => res.status(404).json('no such user'))
+      
+}
+
+module.exports = {
+    handleProfileGet
+}
