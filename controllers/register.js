345cf85cee2f624461f444a50df06b43ee210bce
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-03-25 10:04:29 +0000

    added comments to register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,13 @@
+// function that handles a POST request to register a new user
 const handleRegister = (req, res, db, bcrypt) => {
     const { email, name, password } = req.body;
     if (!email || !name || !password){
         return res.status(400).json('incorrect form submission');
     }
+
+    // generate a hash of the user's password using bcrypt
     const hash = bcrypt.hashSync(password);
+     // use a database transaction to insert the new user into the 'login' and 'users' tables
     db.transaction(trx => {
         trx.insert({
             hash: hash,
@@ -19,6 +23,7 @@ const handleRegister = (req, res, db, bcrypt) => {
                     name: name,
                     joined: new Date()
                 })
+                // if the user was successfully inserted, send the user's data back to the client as a JSON object
                 .then(user => {
                     res.json(user[0]);
             })
@@ -29,6 +34,7 @@ const handleRegister = (req, res, db, bcrypt) => {
     .catch(err => res.status(400).json('unable to register'))
 }
 
+// export the function so it can be used by other modules
 module.exports = {
     handleRegister: handleRegister
 }
