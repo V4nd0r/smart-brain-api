71826e769b2d5163cf510f6f0a84929fe9222de1
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-03-25 09:50:33 +0000

    commented image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,9 +1,11 @@
 const Clarifai = require('clarifai');
 
+// create a new Clarifai application instance with an API key
 const app = new Clarifai.App({
     apiKey:'a5631b3fd6d648ee9b29d320e93cdbb0'
   });
 
+// function that handles API calls to the Clarifai model
 const handleApiCall = (req, res) => {  
     app.models
         .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
@@ -13,6 +15,7 @@ const handleApiCall = (req, res) => {
         .catch(err => res.status(400).json('unable to work with API'))
 }
 
+// function that handles image uploads and updates the user's entry count in the database
 const handleImage = (req, res, db) => {
     const { id } = req.body; 
     db('users').where('id', '=', id)
@@ -24,6 +27,7 @@ const handleImage = (req, res, db) => {
     .catch(err => res.status(400).json('unable to get entries'));
 }
 
+// export the two functions so they can be used by other modules
 module.exports = {
     handleImage,
     handleApiCall
