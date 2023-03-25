c524afc53e1d04952d226114d0a6828ab6984f11
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-03-25 10:29:05 +0000

    added comments to server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+// Import necessary packages and controllers
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+
+// Connect to the database using knex
 const db =knex({
     client: 'pg',
     connection: {
@@ -23,11 +25,14 @@ const db =knex({
     }
   });
 
+// Create an instance of the express application
 const app = express();
 
+// Use CORS and JSON parsing middleware
 app.use(cors())
 app.use(express.json());
 
+// Define API routes and their corresponding controllers
 app.get('/', (req, res)=> {res.send("it is working")})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
@@ -35,6 +40,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) =>{image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) =>{image.handleApiCall(req, res)})
 
+// Start the server listening on the specified port or 3000 if not defined
 app.listen(process.env.PORT || 3000, ()=> {
     console.log(`app is running on  ${process.env.PORT}`);
 })
