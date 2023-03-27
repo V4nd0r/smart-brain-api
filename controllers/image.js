64051d9780e8819bcd039e9206094bea8bc13713
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-03-27 20:12:46 +0100

    Updating Clarifai

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,43 @@
 const Clarifai = require('clarifai');
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+// This will be used by every Clarifai endpoint call
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + PAT);
+
+stub.PostModelOutputs(
+    {
+        user_app_id: {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        model_id: MODEL_ID,
+        inputs: [
+            { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+        ]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            throw new Error(err);
+        }
+
+        if (response.status.code !== 10000) {
+            throw new Error("Post model outputs failed, status: " + response.status.description);
+        }
+
+        // Since we have one input, one output will exist here
+        const output = response.outputs[0];
+
+        console.log("Predicted concepts:");
+        for (const concept of output.data.concepts) {
+            console.log(concept.name + " " + concept.value);
+        }
+    }
+
+);
 
 // create a new Clarifai application instance with an API key
 const app = new Clarifai.App({
